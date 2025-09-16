1e758fb86fc7cdd4026af1cb8acfdefd2717e29f
Christiaan Herrewijn <cherrewi@student.codam.nl>
2025-09-16 20:22:21 +0200

    add validation

--- src/utils.cpp
+++ src/utils.cpp
@@ -3,8 +3,15 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include <cctype>
 
 int str_to_int(std::string number_str) {
+	for (const char c : number_str) {
+		if (!isdigit(c)) {
+			std::cout << "Error: string should only contain digits: " << number_str << std::endl;
+			exit(EXIT_FAILURE);
+		}
+	}
 	if (number_str.size() > 11) { // 10 chars for max_int + 1 char for minus sign
 		// not numbers like 00000000000000042 might not be too large, after all
 		std::cout << "number in equation to large: '" << number_str << "'" << std::endl;
